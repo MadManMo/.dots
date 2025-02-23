ea014aea2ec68762c895afaa14723b2991701125
Mad Man Mo <140839348+MadManMo@users.noreply.github.com>
2025-02-23 13:02:21 -0500

    Delete 3M-OS directory

--- 3M-OS/pkgs/rofi/scripts/drun.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-if (ps -a | grep tofi-drun >/dev/null); then
-  killall -q tofi-drun
-else
-  tofi-drun --drun-launch=true --width 290 --height 290
-fi
--- 3M-OS/pkgs/rofi/scripts/start.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-# initializing wallpaper daemon
-swww init &
-# setting wallpaper
-swww img config/wallpapers/Leyley/Leyleybestgirl.jpg
-
-
