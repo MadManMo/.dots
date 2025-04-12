5dabd13c99304793c4dc6f936bbe4a46c4532ee0
Mad Man Mo <140839348+MadManMo@users.noreply.github.com>
2025-04-12 13:03:10 -0400

    Delete 3M-OS/config/lua directory

--- 3M-OS/config/lua/config/keymaps.lua
+++
@@ -1,9 +0,0 @@
--- lua/config/keymaps.lua
-local map = vim.keymap.set
--- Save and quit
-map("n", "<leader>w", ":w<CR>", { desc = "Save file" })
-map("n", "<leader>q", ":q<CR>", { desc = "Quit" })
--- Terminal keymaps
-map("n", "<leader>t", ":term<CR>", { desc = "Open terminal" })
-map("n", "<leader>ty", ":term yazi<CR>", { desc = "Open yazi in terminal" })
-
--- 3M-OS/config/lua/config/lazy.lua
+++
@@ -1,5 +0,0 @@
--- lua/config/lazy.lua
-return {
-  defaults = { lazy = true },
-  install = { colorscheme = { "kanagawa" } },
-}
